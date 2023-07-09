78c3ce99f6922983bb012f6983beb165f068a679
pinki <pinkiyadav3400@gmail.com>
2023-07-09 14:02:24 +0530

    graphs

--- t2.py/func_t1.py
+++ t2.py/func_t1.py
@@ -1,15 +1,36 @@
 from  turtle import*
 #definition
 
-def square(size,color='red'):
+def hexagone(size,color='red'):
     fillcolor(color)
+    pensize(5)
+    pencolor('red')
     begin_fill()
-    for i in range(4):
+    for i in range(6):
      fd(size)
-     rt(90)
+     rt(72)
      end_fill()
-     square(100)
-     square(50)
-     mainloop()
+hexagone(100)
+def square(size,color='black'):
+   fillcolor(color)
+   pensize(3)
+   pencolor('pink')
+   begin_fill()
+   for i in range(4):
+      fd(size)
+      rt(95)
+      end_fill()
+square(50)
+mainloop()
+
+
+
+
+
+
+
+             
+   
+mainloop()
 
      
